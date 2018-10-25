64662e1c3978011dd917beefd25bb9546b0e76a2
CK <154288465@qq.com>
2018-10-25 22:28:25 +0800

    This is First text

---
+++ MapData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class MapData
+{
+    public List<LevelData> Level;
+}
+
+
+[System.Serializable]
+public class LevelData
+{
+    public int RowCount;
+    public int ColCount;
+    public string[] Map;
+}
